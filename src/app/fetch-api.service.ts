6b4ec4567e04ee8672bc9bc431aa37015ff21344
Michal Kowalczyk <mickowalczyk6@gmail.com>
2022-04-24 20:16:47 +0200

    add service to fetch data from api

---
+++ src/app/fetch-api.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Character } from './interfaces/character';
+import { EpisodesResponse } from './interfaces/api-general-response';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FetchApiService {
+  constructor(private http: HttpClient) {}
+
+  getSeasons(){
+    return this.http.get('https://rickandmorty864.herokuapp.com/season/');
+  }
+
+  getEpisodes(season: number) {
+    return this.http.get<EpisodesResponse>(`https://rickandmorty864.herokuapp.com/season/${season}`);
+  }
+
+  getCharacters(characters: string) {
+    return this.http.get<Character[]>(`https://rickandmortyapi.com/api/character/${characters}`);
+  }
+}
