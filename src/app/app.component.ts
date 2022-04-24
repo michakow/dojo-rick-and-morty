2f72a0d15adf57bf3afc21a1845a912b07ceea45
Michal Kowalczyk <mickowalczyk6@gmail.com>
2022-04-24 20:19:15 +0200

    add template and logic to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FetchApiService } from './fetch-api.service';
+import { Character } from './interfaces/character';
+import { Episode } from './interfaces/episode';
+import { Season } from './interfaces/season';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'dojo-rick-and-morty';
+export class AppComponent implements OnInit {
+  seasons: number[] = [];
+  episodes: Episode[] = [];
+  characters: Character[] = [];
+
+  constructor(private fetchApiService: FetchApiService) {}
+
+  ngOnInit(): void {
+    this.fetchApiService.getSeasons().subscribe(res => {
+      this.seasons = Object.values(res)
+    });
+  }
+
+  showEpisodes(season: number) {
+    this.fetchApiService.getEpisodes(season).subscribe((res) => {
+      this.episodes = res.episodes
+    });
+  }
 }
